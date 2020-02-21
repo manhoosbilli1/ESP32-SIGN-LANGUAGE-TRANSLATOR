fbd3ef9193824938eccf8603ee31d8569483649c
Shoaib <shoaib.mustafa7@hotmail.com>
2020-02-21 21:47:54 +0500

    just an update

--- integrated_code_for_arduino/integrated_code_for_arduino.ino
+++ integrated_code_for_arduino/integrated_code_for_arduino.ino
@@ -1,9 +1,12 @@
-String command,com;
-unsigned long lastTrigger,lastTrigger1, lastTrigger2;              //timers
+//libraries 
+
+
+String command, com;
+unsigned long lastTrigger, lastTrigger1, lastTrigger2;             //timers
 float latitude, longitude;                                         //for gps
+bool playedOnce = false;
 
-
-//flex sensor 
+//flex sensor
 #define fingerPin1 A0
 const int minimumVal1;
 const int maximumVal1;
@@ -27,20 +30,28 @@ const int maximumVal5;
 //-------------
 
 
+//audio and sd card
+
+TMRpcm tmrpcm;
+
+//---------------
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  delay(1000);
-  command_encoder(12.34445565, 44.2944824, "hello");
-  delay(1000);
+  tmrpcm.speakerPin = 9;
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Command:      ");
+  lcd.setCursor(0, 1);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(millis() - lastTrigger > 2000) {                             //will continously print to serial every 2 second with new values 
-    command_encoder(latitude,longitude,com);
+  if (millis() - lastTrigger > 2000) {                            //will continously print to serial every 2 second with new values
+    command_encoder(latitude, longitude, com);
     lastTrigger = millis();
   }
 
--- integrated_code_for_arduino/pose_calculator.ino
+++ integrated_code_for_arduino/pose_calculator.ino
@@ -1,15 +1,116 @@
 void pose_calculator() {
+
+  //getting ADC reading
   int finger1 = analogRead(fingerPin1);
   int finger2 = analogRead(fingerPin2);
   int finger3 = analogRead(fingerPin3);
   int finger4 = analogRead(fingerPin4);
   int finger5 = analogRead(fingerPin5);
 
+  //mapping values 
   finger1 = map(finger1, minimumVal1, maximumVal1, 0,90);            //map the values to certain values
   finger2 = map(finger2, minimumVal2, maximumVal2, 0,90);
   finger3 = map(finger3, minimumVal3, maximumVal3, 0,90);
   finger4 = map(finger4, minimumVal4, maximumVal4, 0,90);
   finger5 = map(finger5, minimumVal5, maximumVal5, 0,90);
+
+  //comparing values 
+
+  if(finger1 == 90 , finger2 == 90 , finger3 ==90 , finger4 == 90, finger5 ==90 ){            // if all fingers are stretched 
+    counter = 0;                                                                              // if counter 0, it wont do anything
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    counter = 1;                                                                              // triggers a function
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    counter = 2;
+  }
+  
+
+  switch(counter) {
+    case 0:
+    
+    break;
+
+    case 1:
+    break;
+
+    case 2:
+    break;
+    
+    /*case 3:
+    break;
+    
+    case 4:
+    break;    
+    
+    case 5:
+    break;
+
+    case 6:
+    break;
+    
+    case 7:
+    break;
+
+    case 8:
+    break;
+
+    case 9:
+    break;
+
+    case 10:
+    break;
+    
+    case 11:
+    break;    
+    
+    case 12:
+    break;                              //extra commands 
+
+    case 13:
+    break;
+    
+    case 14:
+    break;
+    
+    case 15:
+    break;
+    
+    case 16:
+    break; */
+
+    default:           //add a command that would diable audio 
+  }
+  
+  
+  
+  //extra commands
+  /*else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }else if (finger1 == , finger2 == , finger3 == , finger4 == , finger5 == ){
+    
+  }*/
   
   
 }
