c72e407ff9cbfb0a5b78eafa85c65a7e5bcbdeb4
Shoaib <shoaib.mustafa7@hotmail.com>
2020-02-26 22:45:39 +0500

    deleted unnecessary files

--- integrated_code_for_arduino/gps_handler.ino
+++
@@ -1,9 +0,0 @@
-/*void gpsHandler() {
-  while (SerialGPS.available() > 0) {
-    gps.encode(SerialGPS.read());
-    if (gps.location.isUpdated()) {
-      latitude = gps.location.lat();
-      longitude = gps.location.lng();
-    }
-  }
-}*/
